b8433d41f188b36e3dbfeb595bb5323418fe6b39
cslunetta <cslunetta@outlook.com>
2021-04-01 11:11:57 -0500

    Created base files for Tags

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public void AddTag(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTag(int tagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tag> GetAllTags()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTag(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
