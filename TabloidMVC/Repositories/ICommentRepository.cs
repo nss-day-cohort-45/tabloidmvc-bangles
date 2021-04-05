a76605be64ced339cedb64a599dada755ce31e97
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-04-05 12:04:10 -0500

    implemented a delete method for Comments in CommentController.cs

--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -10,6 +10,8 @@ namespace TabloidMVC.Repositories
     public interface ICommentRepository
     {
         void Add(Comment comment);
+        void Delete(int id);
         List<Comment> GetCommentsByPost(int postId);
+        Comment GetCommentById(int id);
     }
 }
